4b281d77698153417d8f0b0db08a5d5dd6f6b2a0
KokLeong Ngoh <klngoh91@hotmail.com>
2024-06-27 16:17:23 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ARM_Template_Training_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
